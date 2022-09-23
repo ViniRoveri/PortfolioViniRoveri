7dd90e76488ff6495d6c28cd26cd59f91a92ba36
ViniRoveri <viniciusroverirocha@gmail.com>
2022-09-23 17:42:12 -0300

    Even Better Header

---
+++ js/header.js
@@ -0,0 +1,5 @@
+itemSobre.addEventListener('click',()=>window.scrollTo(0,100));
+itemProjetos.addEventListener('click',()=>window.scrollTo(0,fimSobre));
+itemEducacao.addEventListener('click',()=>window.scrollTo(0,fimProjetos));
+itemIdiomas.addEventListener('click',()=>window.scrollTo(0,fimEducacao));
+itemLinks.addEventListener('click',()=>window.scrollTo(0,fimIdiomas));
